e48c270810bf6329322e3ccd566ec57a4f8a1368
Bogdan <Bogdankosyanenko@icloud.com>
2026-08-20 17:52:54 +0500

    Added new endpoints read and update

--- src/main/java/org/example/customes/controller/CustomersCt.java
+++ src/main/java/org/example/customes/controller/CustomersCt.java
@@ -2,13 +2,10 @@ package org.example.customes.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.customes.dto.CustCreateDto;
+import org.example.customes.dto.CustPatchDto;
 import org.example.customes.dto.CustResponseDto;
-import org.example.customes.dto.LoginRequest;
 import org.example.customes.service.CustHiberSvc;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -17,13 +14,22 @@ public class CustomersCt {
 
     private final CustHiberSvc custHiberSvc;
 
-    public CustomersCt(CustHiberSvc custHiberSvc){
+    public CustomersCt(CustHiberSvc custHiberSvc) {
         this.custHiberSvc = custHiberSvc;
     }
 
     @PostMapping("/create")
-    public CustResponseDto createClient(@RequestBody CustCreateDto request){
-        return custHiberSvc.create(request);
+    public CustResponseDto createClient(@RequestBody CustCreateDto request) {
+        return custHiberSvc.createInfo(request);
     }
 
+    @GetMapping(value = "/{id}")
+    public CustResponseDto requestClient(@PathVariable Integer id) {
+        return custHiberSvc.requestInfo(id);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public CustPatchDto updateClient(@PathVariable Integer id, @RequestBody CustPatchDto request) {
+        return custHiberSvc.updateInfo(id, request);
+    }
 }
